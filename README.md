21e509877fd8a02803ee7fc161fd12954255cf6d
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-06-29 09:55:33 +0300

    Update the Payment Integrations

---
+++ README.md
@@ -0,0 +1,6 @@
+## 💳 Payment Integration (Chapa)
+
+- Integrated Chapa API to securely handle payments
+- Payments are initiated and verified via Django REST API
+- Booking reference and transaction status stored in the Payment model
+- Celery used to send confirmation emails
--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ListOflocationsList, ListOfhotelsList, ListOfhotelsImagesList, ListOfhotelsReviewsList, ListOfhotelsBookingsList
-    
+from  .views import ListOflocations, ListOfhotels,ListOfhotelsBookings, ini
  
 
 urlpatterns = [
@@ -13,4 +13,6 @@ urlpatterns = [
     path('hotels/<int:hotel_id>/reviews/', ListOfhotelsReviewsList.as_view(), name='hotel_reviews_list'),
     path('hotels/<int:hotel_id>/reviews/<int:pk>/', ListOfhotelsReviewsList.as_view(), name='hotel_review_detail'),
     path('hotels/<int:hotel_id>/bookings/', ListOfhotelsBookingsList.as_view(), name='hotel_bookings_list'),
+    path('payment/initiate/', initiate_payment, name='initiate_payment'),
+    path('payment/verify/', verify_payment, name='verify_payment'),
 ]
